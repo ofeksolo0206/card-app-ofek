216d452592e58be8d6ffdcd35ef3131beac03422
ofeksolo0206 <130289366+ofeksolo0206@users.noreply.github.com>
2023-07-08 17:48:07 +0300

    Delete src directory

--- src/sandbox/components/SecondComponent.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useTheme } from "../../providers/ThemeProvider";
-
-export default function SecondComponent() {
-const {isDark} = useTheme();
-
-  return <div style={{color : isDark && "white"}}>second Component</div>;
-}
