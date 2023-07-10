369120cfaa4a8ace2f82f4e44cc16c4d4371af63
ofeksolo0206 <130289366+ofeksolo0206@users.noreply.github.com>
2023-07-10 12:44:14 +0300

    Update CardBussinesComponent.jsx

--- src/cards/components/card/CardBussinesComponent.jsx
+++ src/cards/components/card/CardBussinesComponent.jsx
@@ -38,6 +38,7 @@ export default function CardBussinesComponent({
           handleDelete={handleDelete}
           handleLike={handleLike}
           cardLikes={card.likes}
+          phone={card.phone}
         />
       </Card>
     </>
